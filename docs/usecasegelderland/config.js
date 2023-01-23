1897cd622328184929271009879cd06d01a9029b
Elisabeth de Vries <elisabeth.kloren@crow.nl>
2023-01-23 08:58:41 +0100

    spec status change

--- docs/usecasegelderland/config.js
+++ docs/usecasegelderland/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
   // this template doesn't use all possible config parameters
   // see https://github.com/stichting-crow/respec/wiki for all options
-  specStatus: "WV",
+  specStatus: "DRAFT",
   postProcess: [], //-- Deze roept de postprocessor aan
 
 
